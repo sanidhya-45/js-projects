098831a20e94c80546208e5d5167192405be23ca
Sanidhya <sanidhyajain45@gmail.com>
2024-06-09 15:30:17 +0530

    Added part 2

---
+++ js_HC/01_basics/datatypes.js
@@ -0,0 +1,21 @@
+// alert(34)  this is nodeJs ....alert was in vanilla js or in live server
+
+console.log("hi") 
+console.log("hi2")  
+
+let name= "Sanidhya"
+let age= 23
+
+let details= {
+    voterId:true,
+    district:"jabalpur",
+    state:"MP",
+    domicile:true,
+    married:false,
+    pincode: 482001,
+    property: null
+}
+
+console.group('User Management') // used in browser to open a group and have related logs together... conosle.groupEnd() can be used to end the group;
+console.log(typeof(undefined)) // will log undefined
+console.log(typeof(null)) // will log object as O/P
